4a0b94d4f98869e25e802e50a3f7f4bbbd399a52
LAPTOP-PATF7J21\Tibame_T14 <henry910119@gmail.com>
2023-05-28 16:11:35 +0800

    改良版的 抱歉各位

--- src/main/java/tw/idv/ixercise/report/dao/impl/reportDaoImpl.java
+++ src/main/java/tw/idv/ixercise/report/dao/impl/reportDaoImpl.java
@@ -1,12 +1,18 @@
 package tw.idv.ixercise.report.dao.impl;
 
 import tw.idv.ixercise.report.dao.reportDao;
+import tw.idv.ixercise.report.entity.Article;
 import tw.idv.ixercise.report.entity.Report;
 
 public class reportDaoImpl implements reportDao {
 
-	@Override
 	public Report findAll(Report clazz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Article selectById(Integer id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
