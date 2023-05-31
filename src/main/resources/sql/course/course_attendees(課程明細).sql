32e10604c15f960984f0664aea181fc9ba240eb3
Jimmy9733 <jimmy97332@gmail.com>
2023-05-31 17:23:35 +0800

    有新增jimmy_personalPage.js

--- src/main/resources/sql/course/course_attendees(課程明細).sql
+++ src/main/resources/sql/course/course_attendees(課程明細).sql
@@ -1,13 +1,12 @@
 CREATE TABLE course_attendees (
-  attendId INT AUTO_INCREMENT,
-  courseId INT,
-  memberId INT(10),
-  attendTime TIMESTAMP,
-  status INT,
-  reason TEXT,
-  paymentStatus INT,
-  commentStatus INT,
-  PRIMARY KEY (attendId)
+`attendId` int NOT NULL AUTO_INCREMENT,
+`courseId` int DEFAULT NULL,
+`memberId` int DEFAULT NULL,
+  `attendTime` timestamp NULL DEFAULT NULL,`status` int NOT NULL DEFAULT '0' COMMENT '1.待審核2.通過3.未通過4:退出',`reason` text NOT NULL,
+  `paymentStatus` int DEFAULT '1' COMMENT '1.未付款\n2.已付款\n3.申請退款\n4:已退款',
+  `commentStatus` int DEFAULT NULL,
+  `course_attendeescol` varchar(45) GENERATED ALWAYS AS (0) VIRTUAL COMMENT '0.未評價1.已評價',
+  PRIMARY KEY (`attendId`)
 );
 
 INSERT INTO course_attendees (courseId, memberId, attendTime, status, reason, paymentStatus, commentStatus)
