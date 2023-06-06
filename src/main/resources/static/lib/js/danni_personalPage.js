2aea147fa08ea0fb550b7d37f013f1854855e255
Dawoo <594danielwu@gmail.com>
2023-06-07 01:40:51 +0800

    完成課程退出功能 差建view

--- src/main/resources/static/lib/js/danni_personalPage.js
+++ src/main/resources/static/lib/js/danni_personalPage.js
@@ -71,13 +71,13 @@ addEventListener("DOMContentLoaded", () => {
 
                     switch (course.courseStatus) {
                         case 1:
-                            statusButtons = `<a class="ms-5 btn justify-content-end small btn-danger mb-1" href="#" role="button">退出</a>`;
+                            statusButtons = `<button class="ms-5 btn justify-content-end small btn-danger mb-1 cancelParticipation" href="#" role="button" >退出</button>`;
                             break;
                         case 2:
-                            statusButtons = `<a class="btn btn-primary" href="#" role="button">前往聊天室　</a>`;
+                            statusButtons = `<button class="btn btn-primary hopIntoChatRoom" href="#" role="button">前往聊天室　</button>`;
                             break;
                         case 3:
-                            statusButtons = `<a class="btn btn-danger" href="#" role="button" disabled>未通過　</a>`;
+                            statusButtons = `<button class="btn btn-danger attendDenied" href="#" role="button" disabled>未通過　</button>`;
                             break;
                         default:
                             // Default buttons when course status doesn't match any case
@@ -87,15 +87,15 @@ addEventListener("DOMContentLoaded", () => {
 
                     course_calendar_list.innerHTML += `
                                             <!-- 一條紀錄====================== -->
-                                        <div href="#" class="list-group-item list-group-item-action" aria-current="true"
+                                        <div href="#" class="list-group-item list-group-item-action courseList" data-attendid="1" aria-current="true"
                                              style="height: 118px;">
                                             <div class="row h-100">
                                                 <div class="col-2 h-100">
                                                     <img src="https://picsum.photos/500/200/?random=10" alt="" width="100px"
                                                          class="overflow-hidden" height="100px" style="object-fit:cover;">
                                                 </div>
-                                                <div class="col-8">
-                                                    <div class="d-flex w-100 justify-content-between">
+                                                <div class="col-8 courseInfoContainer">
+                                                    <div class="d-flex w-100 justify-content-between courseNameContainer">
                                                         <h5 class="mb-1">${course.eventName}</h5>
                                                     </div>
                                                     <p class="mb-1">${course.courseStartDate}</p>
@@ -115,8 +115,48 @@ addEventListener("DOMContentLoaded", () => {
             });
 
 
-        function getContextPath() {
-            return window.location.pathname.substring(0, window.location.pathname.indexOf('/', 2));
-        }
     });
+});
+
+function getContextPath() {
+    return window.location.pathname.substring(0, window.location.pathname.indexOf('/', 2));
+}
+
+function cancelParticipation(e) {
+    console.log(e);
+    console.log(this);
+    // fetch()
+}
+
+
+$("#course-calendar-list").on('click', '.cancelParticipation', function (e) {
+    console.log(e);
+    console.log(this);
+    let cancelCheck = confirm("確認已提出取消報名此課程");
+    if (cancelCheck) {
+        console.log(cancelCheck);
+        let attendId = $(this).closest(".courseList").attr("data-attendid");
+        console.log(attendId);
+        // let update
+        // fetch()
+        let urls = `../courseAttendee/updateStatusById?attendId=${attendId}&status=3`;
+        fetch(urls, {
+            method: 'PUT'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('現在是updateStatusById~');
+                console.log(data);
+                if (!data.successful) {
+                    console.log('修改Status失敗');
+                } else {
+                    console.log('修改Status成功');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    }
+
 });
