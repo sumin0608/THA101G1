66496da4c1e170cd9b0b5cf0c8cc8b707a3c20f4
LAPTOP-PATF7J21\Tibame_T14 <henry910119@gmail.com>
2023-06-08 09:29:18 +0800

    排程器 最後一哩路

---
+++ src/main/java/tw/idv/ixercise/report/control/ScheduleTasks.java
@@ -0,0 +1,22 @@
+package tw.idv.ixercise.report.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import tw.idv.ixercise.report.service.reportServiceImpl;
+
+@Component
+public class ScheduleTasks {
+		
+	@Autowired
+	private reportServiceImpl service;
+	
+	static Integer count=1;
+
+	@Scheduled(cron = "0 2 14 * * ?")
+	public void delete() {
+			service.remove(count);
+			count++;
+	}
+}
